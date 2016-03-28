6a5c01bcae33fb9f69cbb3a21d0da561aef0940f
Mike Conlen <mike@conlen.org>
2016-03-28 08:37:44 -0400

    fixed up the output

--- src/stringoutput.hpp
+++ src/stringoutput.hpp
@@ -31,30 +31,72 @@ namespace stringAlgorithms {
       return os;
    }
 
+   enum outputColors { BLACK = 0, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, NORMAL };
+   char colorStrings[][6] = {
+      "\033[30m",
+      "\033[31m",
+      "\033[32m",
+      "\033[33m",
+      "\033[34m",
+      "\033[35m",
+      "\033[36m",
+      "\033[37m",
+      "\033[0m"
+   };
+
    template<template<typename, typename...> class T, typename C, typename... Args>
-   void displayDiff(T<C, Args...> a, T<C, Args...> b, C delChar = '-')
+   void displayDiff(T<C, Args...> a, T<C, Args...> b, C delIndicator = '-', char delChar = '-')
    {
-      enum outputColors { BLACK = 0, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, NORMAL };
-      char colorStrings[][6] = {
-         "\033[30m",
-         "\033[31m",
-         "\033[32m",
-         "\033[33m",
-         "\033[34m",
-         "\033[35m",
-         "\033[36m",
-         "\033[37m",
-         "\033[0m"
-      };
 
       auto a_cur = a.begin(), b_cur = b.begin();
+
+      std::ostringstream as, bs;
+
+      as << "[ ";
+      bs << "[ ";
+
+      while(a_cur != a.end() && b_cur != b.end()) {
+         if(*a_cur == delIndicator) {
+            as << delChar;
+            bs << colorStrings[CYAN] << *b_cur << colorStrings[NORMAL];
+         } else if(*b_cur == delIndicator) {
+            as << colorStrings[CYAN] << *a_cur << colorStrings[NORMAL];
+            bs << delChar;
+         } else if(*a_cur != *b_cur) {
+            as << colorStrings[RED] << *a_cur << colorStrings[NORMAL];
+            bs << colorStrings[RED] << *b_cur << colorStrings[NORMAL];
+         } else {
+            as << *a_cur;
+            bs << *b_cur ;
+         }
+         a_cur++; b_cur++;
+         if(a_cur == a.end() || b_cur == b.end()) {
+            as << " ]";
+            bs << " ]";
+         } else {
+            as << ", ";
+            bs << ", ";
+         }
+      }
+
+
+      std::cout << "a = " << as.str() << std::endl;
+      std::cout << "b = " << bs.str() << std::endl;
+      std::cout << std::endl;
+      return;
+   }
+
+   template<>
+   void displayDiff(std::string a, std::string b, char delIndicator, char delChar)
+   {
+     auto a_cur = a.begin(), b_cur = b.begin();
 
       std::ostringstream as, bs;
 
       while(a_cur != a.end() && b_cur != b.end()) {
          if(*a_cur == delChar) {
             as << *a_cur;
-            bs << colorStrings[CYAN] << *b_cur;
+            bs << colorStrings[CYAN] << *b_cur << colorStrings[NORMAL];
          } else if(*b_cur == delChar) {
             as << colorStrings[CYAN] << *a_cur << colorStrings[NORMAL];
             bs << *b_cur;
