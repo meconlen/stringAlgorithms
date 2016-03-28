2951f9dd16e6e8265512d41ccec46749b6e21ad9
Mike Conlen <mike@conlen.org>
2016-03-28 09:03:22 -0400

    clean up some variables and add a benchmark

--- src/hirschberg.hpp
+++ src/hirschberg.hpp
@@ -60,63 +60,64 @@ namespace stringAlgorithms {
       return top;
    }
 
-   namespace {
-      template<typename I>
-      int_fast64_t partition(I lb, I le, I rb, I re) {
-         auto L_cur = lb;
-         auto R_cur = re - 1;
-         auto ymid = 0;
-         auto curMax = *L_cur + *R_cur;
-         auto size = std::distance(lb, le);
-         L_cur++; R_cur--;
-         for(auto i = 1; i < size; i++) {
-            auto curSum = *L_cur + *R_cur;
-            if(curSum > curMax) {
-               ymid = i;
-               curMax = curSum;
-            }
-            L_cur++; R_cur--;
+   // I should probably check distances and throw an exception here if they are not equal
+   // however this is tightly coupled to Hirschberg() so I'm not too concerned.
+   // might be interesting to benchmark though 
+
+   template<typename I>
+   int_fast64_t partition(I left_begin, I left_end, I right_begin, I right_end) {
+      auto left_cur = left_begin;
+      auto right_cur = right_end - 1;
+      auto y_mid = 0;
+      auto current_max = *left_cur + *right_cur;
+      auto size = std::distance(left_begin, left_end);
+      left_cur++; right_cur--;
+      for(auto i = 1; i < size; i++) {
+         auto current_sum = *left_cur + *right_cur;
+         if(current_sum > current_max) {
+            y_mid = i;
+            current_max = current_sum;
          }
-         return ymid;
+         left_cur++; right_cur--;
       }
+      return y_mid;
    }
 
    template<typename I, typename BI, typename F>
-   void Hirschberg(I xb, I xe, I yb, I ye, BI &&wb, BI &&zb,
-      F &&score_function, const typename std::iterator_traits<I>::value_type delChar = '-', typename PP<F, I>::type ID = -1)
+   void Hirschberg(I x_begin, I x_end, I y_begin, I y_end, BI &&w_back, BI &&z_back, F &&score_function, 
+      const typename std::iterator_traits<I>::value_type deleted_value = '-', typename PP<F, I>::type ID = -1)
    {
-      // typedef typename PP<F, I>::type P;
+      int_fast64_t x_size = std::distance(x_begin, x_end);
+      int_fast64_t y_size = std::distance(y_begin, y_end);
 
-      int_fast64_t x_size = std::distance(xb, xe);
-      int_fast64_t y_size = std::distance(yb, ye);
       if(x_size == 0) {
-         std::fill_n(wb, y_size, delChar);
-         std::copy(yb, ye, zb);
+         std::fill_n(w_back, y_size, deleted_value);
+         std::copy(y_begin, y_end, z_back);
       } else if(y_size == 0) {
-         std::copy(xb, xe, wb);
-         std::fill_n(zb, x_size, delChar);
+         std::copy(x_begin, x_end, w_back);
+         std::fill_n(z_back, x_size, deleted_value);
       } else if(x_size == 1 || y_size == 1) {
-         NeedlemanWunsch(xb, xe, yb, ye, wb, zb, score_function, delChar, ID);
+         NeedlemanWunsch(x_begin, x_end, y_begin, y_end, w_back, z_back, score_function, deleted_value, ID);
       } else {
-         auto xmid = x_size / 2;
+         auto x_mid = x_size / 2;
 
-         auto ScoreL = nwScore(xb, xb+xmid, yb, ye, score_function, ID);
+         auto ScoreL = nwScore(x_begin, x_begin + x_mid, y_begin, y_end, score_function, ID);
          
          std::vector<typename std::iterator_traits<I>::value_type> y_reverse;
-         std::copy(yb, ye, back_inserter(y_reverse));
+         std::copy(y_begin, y_end, back_inserter(y_reverse));
          std::reverse(y_reverse.begin(), y_reverse.end());
 
          std::vector<typename std::iterator_traits<I>::value_type> x_reverse;
-         std::copy(xb+xmid, xe, back_inserter(x_reverse));
+         std::copy(x_begin + x_mid, x_end, back_inserter(x_reverse));
          std::reverse(x_reverse.begin(), x_reverse.end());
 
 
          auto ScoreR = nwScore(x_reverse.begin(), x_reverse.end(), y_reverse.begin(), y_reverse.end(), score_function, ID);
 
-         auto ymid = partition(ScoreL.begin(), ScoreL.end(), ScoreR.begin(), ScoreR.end());
+         auto y_mid = partition(ScoreL.begin(), ScoreL.end(), ScoreR.begin(), ScoreR.end());
 
-         Hirschberg(xb, xb+xmid, yb, yb+ymid, wb, zb, score_function, delChar, ID);
-         Hirschberg(xb+xmid, xe, yb+ymid, ye, wb, zb, score_function, delChar, ID);
+         Hirschberg(x_begin, x_begin + x_mid, y_begin, y_begin + y_mid, w_back, z_back, score_function, deleted_value, ID);
+         Hirschberg(x_begin + x_mid, x_end, y_begin + y_mid, y_end, w_back, z_back, score_function, deleted_value, ID);
 
       }
    }
